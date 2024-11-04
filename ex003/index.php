4ec6eef52425ea60507f6cddd3953e5c76b6b3df
Mário Cézar <163690025+mcazon@users.noreply.github.com>
2024-11-04 06:42:03 -0300

    Create index.php

---
+++ ex003/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+   //     $v = 0b110;
+  //      var_dump($v);
+        $vet = [2, 5, 7, 11];
+        var_dump($vet);
+     //   echo "o vetor é $vet ";
+    ?>
+    
+</body>
+</html>
